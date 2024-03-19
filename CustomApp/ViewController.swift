c30a9196ab7f41af023effcf615aa27ce5e85aaf
miteshAnnex <mpatel@annexcloud.com>
2024-03-19 17:26:27 +0530

    added new log file

--- CustomApp/ViewController.swift
+++ CustomApp/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("code chnages.")
         printContent("After uploade changes in development branch")
+        print("uploaded on remote repo")
     }
 
 
